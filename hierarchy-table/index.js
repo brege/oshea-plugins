526dbd86445ee7e989211fe3a59f64901a728057
Wyatt Brege <wyatt@brege.org>
2025-05-24 17:26:05 -0400

    Add hiearchy-table plugin

---
+++ hierarchy-table/index.js
@@ -0,0 +1,13 @@
+// ../md-to-pdf-plugins/hierarchy-table/index.js
+class HierarchyTableHandler {
+    constructor(coreUtils) {
+        // coreUtils contains { DefaultHandler, markdownUtils, pdfGenerator }
+        this.handler = new new coreUtils.DefaultHandler();
+    }
+
+    async generate(data, pluginSpecificConfig, globalConfig, outputDir, outputFilenameOpt, pluginBasePath) {
+        console.log(`INFO (HierarchyTableHandler): Processing for plugin '${pluginSpecificConfig.description || 'hierarchy-table'}'`);
+        return this.handler.generate(data, pluginSpecificConfig, globalConfig, outputDir, outputFilenameOpt, pluginBasePath);
+    }
+}
+module.exports = HierarchyTableHandler;
